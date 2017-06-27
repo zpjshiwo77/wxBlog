abad7c89d51205f79b9aad2946bb6f1a18da4f39
page <page@be-biu.com>
2017-06-27 15:15:06 +0800

    绑定后台数据，新增点赞功能

--- pages/food/food.js
+++ pages/food/food.js
@@ -1,14 +1,18 @@
 //index.js
 var nav = require('../../utils/nav.js'); //获取导航的实例
-var foodData = require('../../utils/food_data.js'); //获取食物的信息
-
+var page = 1;
+var foodlist = [];
+var requestFlag = true;
+var likeFlag = true;
+var NowShare = '/pages/food/food';
 Page({
   data: {
     nav:nav.NavF(),
-    m_Cont:foodData.foodData
+    m_Cont:foodlist
   },
   onLoad: function () {
     this.Nav_Init();
+    this.GetFoodList();
   },
   Nav_Init: function(){
     var thisNav = nav.NavF();
@@ -22,7 +26,75 @@ Page({
   onShareAppMessage: function () {
     return {
       title: "美食 - Musical美食",
-      path: '/pages/food/food'
+      path: NowShare
+    }
+  },
+  GetFoodList: function(data){
+    var that = this;
+    var data = {
+      method:"GetFoodList",
+      page:page
+    };
+    this.requsetFunc(data,function(res){
+      requestFlag = true;
+      page++;
+      var list = res.data.result.foods;
+      foodlist.push.apply(foodlist,list);
+      that.setData({
+        m_Cont:foodlist
+      });
+    });
+  },
+  likeThisFood: function(event){
+    var that = this;
+    var id =  event.currentTarget.dataset.id;   
+    if(likeFlag){
+      var data = {
+        method:"addAHite",
+        id:id
+      };
+       for(var i = 0;i < foodlist.length;i++){
+        if(foodlist[i].id == id) foodlist[i].like++;
+      }
+      that.setData({
+        m_Cont:foodlist
+      });
+      this.requsetFunc(data,function(res){
+        likeFlag = false;
+      });
+    }
+    else{
+      wx.showModal({
+        title: '感谢您的支持',
+        content: '您已经点过赞了',
+      });
+    }
+  },
+  // setNowId: function(event){
+  //   var id =  event.currentTarget.dataset.id;
+  //   NowShare = '/pages/food_detail/food_detail?id=' + id;
+  // },
+  requsetFunc: function(data,callback){
+    var that = this;
+    if(requestFlag){ 
+      requestFlag = false;
+      wx.request({
+        url: 'https://www.seventh77.com/modal/food/food.php',
+        method: 'GET',
+        data: data,
+        header: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        success: function(res) {
+          if(res.data.errorCode == 0){
+            callback(res);
+          }
+        },
+        fail: function(){
+          requestFlag = true;
+          console.log("fail");
+        }
+      });
     }
   }
 })
--- pages/food_detail/food_detail.js
+++ pages/food_detail/food_detail.js
@@ -2,15 +2,14 @@
 var nav = require('../../utils/nav.js'); //获取导航的实例
 var foodData = require('../../utils/foodDetail_data.js'); //获食物详细信息的实例
 var food,fid;
-
+var requestFlag = true;
 Page({
   data: {
     nav:nav.NavF(),
   },
   onLoad: function (options) {
     this.Nav_Init();
-    food = this.requestData(options.id);
-    this.page_Init(food);
+    this.requestData(options.id);
   },
   page_Init: function(data){
     this.setData({
@@ -18,17 +17,25 @@ Page({
     });
   },
   requestData: function(id){
-    var data = foodData.foodData;
-    var myfood;
-    fid = id;
-
-    for(var i = 0;i < data.length;i++){
-      if(id == data[i].id){
-        myfood = data[i];
-      }
+    var that = this;
+    var data = {
+      method:"GetFoodDetail",
+      id:id
+    };
+    that.requsetFunc(data,function(res){
+      food = that.dealData(res.data.result);
+      that.page_Init(food);
+    });
+  },
+  dealData: function(data){
+    var step = data.step;
+    for(var i = 0;i < step.length;i++){
+      var item = step[i].cont;
+      item = item.split(";");
+      item.pop();
+      step[i].cont = item;
     }
-
-    return myfood;
+    return data;
   },
   Nav_Init: function(){
     var thisNav = nav.NavF();
@@ -43,6 +50,30 @@ Page({
     return {
       title: food.name + " - Musical美食",
       path: '/pages/food_detail/food_detail?id='+fid
+    }
+  },
+  requsetFunc: function(data,callback){
+    var that = this;
+    if(requestFlag){ 
+      requestFlag = false;
+      wx.request({
+        url: 'https://www.seventh77.com/modal/food/food.php',
+        method: 'GET',
+        data: data,
+        header: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        success: function(res) {
+          if(res.data.errorCode == 0){
+            requestFlag = true;
+            callback(res);
+          }
+        },
+        fail: function(){
+          requestFlag = true;
+          console.log("fail");
+        }
+      });
     }
   }
 })
